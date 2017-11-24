26c22622778a5a7e89152419d33da3e85c38e625
Nikita Savkin <savkin@arcsinus.ru>
2017-11-24 15:20:53 +0300

    Мелкие изменения

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ module.exports = {
    * pluralize(3, ["пассажир", "пассажира", "пассажиров"]) // return 'пассажира'
    */
   pluralize(number, titles) {
+    
     number = Math.abs(number);
     const cases = [2, 0, 1, 1, 1, 2];
     let index = 0;
